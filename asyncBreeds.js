5db56eafdcdac9549bb43ca102e2e80c65f31ff7
Adam Hirzalla <adam.hirzalla@carleton.ca>
2021-11-03 00:07:41 -0400

    M&C'ing both sync and async functions

--- asyncBreeds.js
+++ asyncBreeds.js
@@ -2,16 +2,14 @@
 const fs = require('fs');
 
 const breedDetailsFromFile = function(breed, done) {
-  console.log('breedDetailsFromFile: Calling readFile...');
   fs.readFile(`./data/${breed}.txt`, 'utf8', (error, data) => {
-    console.log("In readFile's Callback: it has the data.");
     // ISSUE: Returning from *inner* callback function, not breedDetailsFromFile.
     if (!error) done(data);
+    else done(undefined);
   });
 };
 const outputDetailsFromFile = (data)=>{
-  console.log('Return Value: ', data); // => will NOT print out details, instead we will see undefined!
+  console.log('Return Value:', data); // => will NOT print out details, instead we will see undefined!
 };
 
-// we try to get the return value
-breedDetailsFromFile('Bombay', outputDetailsFromFile);
+module.exports = breedDetailsFromFile;
